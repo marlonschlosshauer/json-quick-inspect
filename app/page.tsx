01ef6c9294b90f54247a70eed5463d6c8594fa3c
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-06-28 20:03:51 +0200

    Update text

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,23 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Inspect your .json file in the browser dev tools",
+  title: "Play with your .json",
   description:
-    "Drop in your .json file and quickly iterate with it in the developer console.",
+    "Drop in your .json file and quickly iterate from with in the developer console.",
 };
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white">
       <div className="flex flex-col gap-5 max-w-md">
-        <h1 className="text-2xl">Inspect</h1>
+        <h1 className="text-2xl">Play with your .json</h1>
         <p>
-          Quickly inspect the content of a <code>.json</code> file from inside
-          of the developer console.
+          Drop in your <code>.json</code> file and quickly iterate from with in
+          the developer console.
         </p>
         <p>
           No need to create a <code>.js</code> file just to filter out values
-          from an Array. Just press <kbd>Cmd+Option+I</kbd> (or<kbd>F12</kbd>,{" "}
+          from an Array. Just press <kbd>Cmd+Option+I</kbd> (or <kbd>F12</kbd>,{" "}
           <kbd>Ctrl+Shift+I</kbd>) and iterate quickly.
         </p>
         <p>
@@ -29,8 +29,9 @@ export default function Page() {
             href="https://github.com/marlonschlosshauer/json-quick-inspect"
             target="_blank"
           >
-            source code here.
+            source code here
           </Link>
+          .
         </p>
         <div className="mt-4">
           <JsonFileInput />
